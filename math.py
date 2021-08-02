0894de7a1c1ef29fce36a7af232fae98b162e866
Manju-12 <65105362+Manju-12@users.noreply.github.com>
2021-08-02 14:33:03 +0530

    update math.py

--- math.py
+++ math.py
@@ -1,1 +1,19 @@
-print('hello')
+#add function
+def add(x,y):
+  return x+y
+
+#subtract function
+def subtract(x,y):
+  if x>y:
+    return x-y
+  else:
+    return y-x
+
+
+#multiply function
+def multiply(x,y):
+  pass
+
+#devide function
+def divide(x,y):
+  return x/y
